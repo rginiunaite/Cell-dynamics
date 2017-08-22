03316ee8aa3dfedd234172e076445e8f7587ef4b
rginiunaite <rasaginiu@gmail.com>
2017-08-22 17:37:17 +0100

    max entr + gillespie with hexagonal lattice

--- maximum_entropy_new_version.m
+++ maximum_entropy_new_version.m
@@ -1,4 +1,4 @@
-% combination of Gillespie (40 realisations), logistic and Runge-Kutta with
+% combination of Gillespie, logistic and Runge-Kutta with
 % maximum entropy approximation in 1D, using fsolve for step 3 in maximal
 % entropy scheme
 
@@ -18,7 +18,7 @@ Pp = 0.5; % proliferation rate per unit time of giving rise to another agent
 
 Pd = 0.1; % death rate per unit time
 
-S = load('gillespie1D_large_initial_Pp05_Pd01.mat'); % load the data for a particular choice of Pp and Pd, note that L=100 is fixed
+S = load('gillespie2D_large_initial_Pp05_Pd01.mat'); % load the data for a particular choice of Pp and Pd, note that L=100 is fixed
 
 TimeReductionFactor = 2; % when death rate is increase, this has to be increased as well
 
@@ -72,10 +72,40 @@ gammaA0 = ones(L,max_iter_gamma); % size of gamma A0
 % initial conditions for gamma, first L/2 correspond to gamma(A,A), the other L/2, gamma(A,0)
 
 
-    for i = 1:L
-        gammaAA(i) = 3.89*S.save_cA0^3;
-        gammaA0(i) = 3.89*S.save_cA0*(1-S.save_cA0)^2; 
+    for i = 1:L-1
+         gammaAA(i) = 3.89*S.save_cA0^3; % worked for these, old data
+         gammaA0(i) = 3.89*S.save_cA0*(1-S.save_cA0)^2; 
+%          gammaAA(i) = 4.13*S.save_cA0^3; % worked for these
+%          gammaA0(i) = 4.13*S.save_cA0*(1-S.save_cA0)^2;          
+ 
+          gammaAA(i) = 1.0122*(1-S.save_cA0); % worked for these
+          gammaA0(i) = 1.0122*S.save_cA0;   
+
+%          gammaAA(i) = 1-S.save_cA0^1; % worked for these
+%          gammaA0(i) = S.save_cA0;%(1-S.save_cA0)^1; 
+
+
+%%%%%%%%%%%%%% try to choose initial conditions based on the KSA
+%%%%%%%%%%%%%% approximation of triple, not a good idea
+
+%     %%% doubles from Power 1 approx, they are similar to Gillespie
+%     Data = load('gillespie1D_large_initial_Pp05_Pd01_doubles.mat');
+%     pairs = Data.save_doubles;
+%     den = S.save_density(length(S.save_density));
+%     
+%     gammaAA(i) = (pairs(i)+pairs(i+2))/(1- den);
+% 
+%     gammaA0(i) = sqrt((den-pairs(i+1))*(den-pairs(2)))/den;
+
+
     end
+    
+%     gammaAA(L) = (pairs(L+1)+pairs(L))/(1- den);
+% 
+%     gammaA0(L) = sqrt((den-pairs(L+1))*(den-pairs(2)))/den;  
+    
+    
+    
 
 %     gamma(1:L,1) = gammaAA(:,1);
 %     gamma(L+1:2*L,1) = gammaA0(:,1);
@@ -119,10 +149,8 @@ end
 % use fsolve to find new gamma
 final = size(y(1,:),2);
 
-options = optimoptions('fsolve','MaxIterations',10000, 'FunctionTolerance',1e-6,'MaxFunctionEvaluations',10000,'StepTolerance',1.0000e-08);
+options = optimoptions('fsolve','MaxIterations',10000, 'FunctionTolerance',1e-3,'MaxFunctionEvaluations',10000,'StepTolerance',1.0000e-06);
 
-[xA0,fval] = fsolve(@(gamma_unk)step3A0(gamma_unk,y(2:L+1,final),L,y(1,final)),gammaA0(:,count),options);
-a=2;
 
 [xAA,fval] = fsolve(@(gamma_unk)step3AA(gamma_unk,y(2:L+1,final),L,y(1,final)),gammaAA(:,count),options);
 [xA0,fval] = fsolve(@(gamma_unk)step3A0(gamma_unk,y(2:L+1,final),L,y(1,final)),gammaA0(:,count),options);
@@ -130,16 +158,34 @@ a=2;
 gammaAA(:,count+1) = xAA;
 gammaA0(:,count+1) = xA0;   
 
+
 % gamma(1:L,count+1) = gammaAA(:,count+1);
 % gamma(L+1:2*L,count+1) = gammaA0(:,count+1);
 
 
 count = count +1; % update count, it is 2 now
 
-tol = 5; % tolerance for the convergence of gamma
+
+for i = 2 : N
+
+     t = t + dt;
+
+     k1 = dynamics (t,y(:,i-1),L,Pm,Pp,Pd,gammaAA(:,count),gammaA0(:,count));
+     k2 = dynamics (t+dt/2,y(:,i-1)+dt/2 * k1,L,Pm,Pp,Pd,gammaAA(:,count),gammaA0(:,count));
+     k3 = dynamics (t+dt/2,y(:,i-1)+dt/2 * k2,L,Pm,Pp,Pd,gammaAA(:,count),gammaA0(:,count));
+     k4 = dynamics (t+dt, y(:,i-1) + dt*k3,L,Pm,Pp,Pd,gammaAA(:,count),gammaA0(:,count));
+
+     y(:,i) = y(:,i-1) + dt/6 * (k1 + 2*k2 + 2*k3 + k4);  
+
+     store_time_Runge(i) = t;
+end
 
 
-while norm (gammaAA(:,count)-gammaAA(:,count-1))>tol 
+
+tol = 3; % tolerance for the convergence of gamma
+
+
+while norm (gammaA0(:,count)-gammaA0(:,count-1))>tol 
     
 
     % Runge Kutta method to solve ODEs to find density and pair densities
@@ -187,7 +233,7 @@ y_rescaled = Pp/(Pp-Pd) * y;
 
 hold on
 plot(rescaled_time,y_rescaled(1,:),'LineWidth', 3)
-h_legend = legend('Gillespie 1D','Logistic growth','Maximum entropy');
+h_legend = legend('Gillespie 2D','Logistic growth','Maximum entropy');
 set(h_legend,'FontSize',14)
 xlabel('rescaled time','FontSize',14)
 ylabel('rescaled density','FontSize',14)
@@ -215,7 +261,7 @@ function deriv = dynamics(t,y,Ln,Pm,Pp,Pd,gammaAA,gammaA0)
     end
 
     deriv(Ln+1) = (Pm*(y(Ln) + y(2) - 2* y(Ln+1)) - 2 * Pd * y(Ln+1) +...
-            Pp * (gammaA0(Ln)*gammaA0(1)*(gammaAA(Ln-1)+gammaAA(Ln-1)))); % for the last one, since we have
+            Pp * (gammaA0(Ln)*gammaA0(1)*(gammaAA(Ln-1)+gammaAA(1)))); % for the last one, since we have
         % periodic boundary condition L+1 neighbour is L-1 neighbour, i.e. y(2)
     
 end
